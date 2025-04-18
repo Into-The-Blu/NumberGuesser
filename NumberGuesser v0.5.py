39eaa7891ff30a06ac32141d5e4d3e13bf50c43b
Into-The=Blu <jasmine.r.timson@gmail.com>
2025-04-18 03:39:04 +0100

    Code commented, README added, test file removed

--- NumberGuesser v0.5.py
+++ NumberGuesser v0.5.py
@@ -1,10 +1,12 @@
 import random
 import sys
-#import json
+#import json 
 #import os
 
 scoreboard = []
 
+#requests user input to decide the difficulty. 
+#each case returns a number of chances, and assigns a score multiplier to the diff variable
 def getDiff():
     print(f"\nWhat difficulty would you like to play on?\n1. Easy (10 guesses)\n2. Medium (5 guesses)\n3. Hard (3 guesses)")
     global diff
@@ -23,15 +25,17 @@ def getDiff():
                 print("I didn't recognise that input, could you retype?")
                 continue
 
+#main game body - main loop repeats for each guess, and is only broken by a win or lose condition
+#if-elif-else statements react to the user guessing correctly, wrong and above, or wrong and below
 def guesrBody():
-    chances = getDiff()
+    chances = getDiff() #outputs no. of chances based on user difficulty input
     number = random.randint(1,100)
     print(f"\nAlright! Make your first guess.\n")
     for i in range(chances):
         x = int(input(f"\nInput:").strip())
-        if x == number:
+        if x == number: #checks for a win condition
             global score
-            score = ((chances-(i+1))/chances)*100*diff
+            score = ((chances-(i+1))/chances)*100*diff #(chances left/total chances)*100*difficulty multiplier
             score = int(round(score))
             scoreboardCalc()
             if i + 1 == 1:
@@ -42,12 +46,12 @@ def guesrBody():
             gameFinished = True
             menu()
             break
-        elif x > number:
+        elif x > number: #checks for a wrong and above
             if chances - (i+1) > 1:
                 print(f"Too high! You have {chances-(i+1)} attempts left.\n")
             else: 
                 print(f"Too high! You only have {chances-(i+1)} attempt left!\n")
-        else:
+        else: #checks for a wrong and below
             if chances - (i+1) > 1:
                 print(f"Too low! You have {chances-(i+1)} attempts left.\n")
             else:
@@ -58,13 +62,14 @@ def guesrBody():
         print(f"Uh oh! You've run out of attempts.")
         menu()
 
+#gives the user 3 options: new game, scoreboard, and exit. responds to user input
 def menu():
     print(f"\nWhat would you like to do?\n\nMenu:\n1. New Game\n2. View Scoreboard\n3. Exit")
     while True:
         match str(input(f"\nInput:")):
-            case "1":
+            case "1": #starts the game
                 guesrBody()
-            case "2":
+            case "2": #displays scoreboard
                 print(f"\nScoreboard:\n-----------")
                 for i in range(len(scoreboard)):
                     print(f"{i + 1}.  {scoreboard[i]}")
@@ -72,7 +77,7 @@ def menu():
                     print(f"10. {scoreboard[9]}")
                 print("-----------")
                 break
-            case "3":
+            case "3": #exits game
                 print("Thank you for playing! Enter any input to exit.")
                 if input() == True:
                     sys.exit()
@@ -81,13 +86,14 @@ def menu():
                 continue
     menu()        
 
+#inserts score into scoreboard, then trims index to 10 if >10 
 def scoreboardCalc():
     scoreboard.append(score)
     scoreboard.sort(reverse = True)
     if len(scoreboard) >= 11:
         del scoreboard[10]
 
-print(f"Welcome to my Number Guessing Game!")
-
+#program push-off point
+print(f"Welcome to my Number Guessing Game!\nI will think of a number between 1 and 100, and you have a set amount of chances to guess it.")
 menu()
 
---
+++ README.md
@@ -0,0 +1,27 @@
+Thank you for taking the time to play this!
+
+This is my roadmap.sh Number Guesser project entry - it (should) meet the following requirements:
+
+1. When the game starts, it should display a welcome message along with the rules of the game.
+2. The computer should randomly select a number between 1 and 100.
+3. User should select the difficulty level (easy, medium, hard) which will determine the number of chances they get to guess the number.
+4. The user should be able to enter their guess.
+5. If the user’s guess is correct, the game should display a congratulatory message along with the number of attempts it took to guess the number.
+6. If the user’s guess is incorrect, the game should display a message indicating whether the number is greater or less than the user’s guess.
+7. The game should end when the user guesses the correct number or runs out of chances.
+
+Additional features include a menu, a scoreboard + difficulty based score system, with scoreboard persistence/JSON loading not yet implemented, thereby sort of meeting 2 of the challenge requirements:
+
+1. Allow the user to play multiple rounds of the game (i.e., keep playing until the user decides to quit). You can do this by asking the user if they want to play again after each round.
+
+and
+
+4. Keep track of the user’s high score (i.e., the fewest number of attempts it took to guess the number under a specific difficulty level).
+
+Code is moderately commented, apologies if it's a mess.
+
+Any improvments/comments don't hesitate to drop me a message!
+
+Jazz :3
+
+
